e3aac866c01cff524c4f08f9fe0d0ea3d3533639
mjarkk <mkopenga@gmail.com>
2019-06-18 10:47:44 +0200

    Updated readme to the v0.2 version

--- mkRelease.sh
+++ mkRelease.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
 
-# remove old files
-rm -rf releases
-
-# Bulid the go project
-GOOS=linux go build
-
-# make releases folder and copy over the needed files
-mkdir releases
-cp haproxy-check-api releases
-cp Dockerfile releases
-
-# zip the files
-cd releases
-zip release.zip haproxy-check-api Dockerfile
-cd ..
-
-# Cleanup some files
-rm releases/Dockerfile releases/haproxy-check-api
-
-echo "----------------------------"
-echo "Created releases/release.zip"
-echo "----------------------------"
+docker save haproxy-check-api > ./haproxy-check-api.tar
